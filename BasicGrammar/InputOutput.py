23477f9563588e46b2ebb904a01a8e5880836aae
ShiinaOrez <shiina_orez@qq.com>
2019-02-25 21:14:56 +0800

    [feat]InputOutput Class 1

---
+++ BasicGrammar/InputOutput.py
@@ -0,0 +1,7 @@
+startString = "Hello, python!"
+print(startString)
+
+name = input("What's your name?: ")
+age = int(input("How old are you?: "))
+
+print("Hello,", name+". And you're", age, "year's old.")
